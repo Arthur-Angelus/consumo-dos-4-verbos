24d7bd680ea99ef790291fe56e573f16a3d19fe7
ARTHUR ANGELUS ANDRADE DE ALMEIDA <25132783@CFP127ALUNO.LOCAL>
2025-11-17 07:40:36 -0300

    comentando as funções

--- contatos.js
+++ contatos.js
@@ -71,7 +71,7 @@ const novoContato = {
         "cidade": "Niteroi"
 }
 
-lerContatos()
-criarContato(novoContato)
-atualizarContato(4, novoContato)
-deletarContato()
+//lerContatos()
+//criarContato(novoContato)
+//atualizarContato(4, novoContato)
+//deletarContato()
